5423585c1c6c647f3d897b7882ffefbdbcb64620
David Tan <davified@gmail.com>
2016-11-18 06:52:52 +1100

    added additional filter search terms

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ function generateRandomInteger (min, max) {
 
 function checkIfBreakdown (tweetText) {
   tweetText = tweetText.toLowerCase()
-  if (tweetText.match('mrt breakdown|mrt disruption|breakdown|delay|delayed|delays|disruption|train fault|no train service') && !tweetText.match('bangkok|thailand|bkk')) {
+  if (tweetText.match('mrt breakdown|mrt disruption|breakdown|delay|delayed|delays|disruption|train fault|no train service') && !tweetText.match('bangkok|thailand|bkk|busan|djmrt')) {
     breakdownTweetsCount++
     breakdownTweetsArray.push(tweetText)
     console.log(`${anyTrainBreakdown}: ${tweetText}`)
